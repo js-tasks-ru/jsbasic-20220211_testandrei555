fd6ed101bf571d21c2971fcb4bc7edb99f5d9842
barrym0re <TestAndrei555@yandex.ru>
2022-03-11 04:10:34 +0500

    module-2/module-3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,7 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split("-");
+  for (i = 1; i < arr.length; i++) {
+    arr.splice(i, 1, arr[i][0].toUpperCase() + arr[i].slice(1));
+  }
+  return arr.join("");
 }
